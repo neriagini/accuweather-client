b24dbbf04a48cf2430d6e588ec302983b8505581
neriagoren <neriag@gini-apps.com>
2022-08-03 16:51:38 +0300

    added fetch location button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+
 
 function App() {
+  const getEvents = async () => {
+      await axios.get('http://localhost:8080/list-events').then(response => {
+        console.log(response)
+      })
+  }
+  const getLocationKey = async () => {
+    await axios.get('http://dataservice.accuweather.com/locations/v1/cities/search?apikey=b9qlnPQZvXqa7GdY9JTN9aqQoISsyeHi&q=tel%20aviv')
+    .then(response => {
+      console.log(response.data)
+    })
+  }
   return (
     <div className="App">
-      Hello World!
+      <p className='text-[25px] font-bold m-10'> Google Calendar API </p>
+      <button className='text-white border-2 p-2 rounded-xl bg-blue-500' onClick={getEvents}> Fetch Events </button>
+      <p className='text-[25px] font-bold m-10'> AccuWeather API </p>
+      <button disabled className='text-white border-2 p-2 rounded-xl bg-blue-500' onClick={getLocationKey}> Fetch Location Key </button>
     </div>
   );
 }
