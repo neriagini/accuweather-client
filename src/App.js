b256730b6163379df16505e93221e7ffc2135fd2
neriagoren <neriag@gini-apps.com>
2022-08-03 19:17:39 +0300

    added search field for city search

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import './App.css';
 import axios from 'axios';
+import { useState } from 'react';
 
 
 function App() {
+
+  const [location, setLocation] = useState("tel aviv");
+
+  const onLocationChange = (event) => {
+    setLocation(event.target.value);
+  }
+
   const getEvents = async () => {
       await axios.get('http://localhost:8080/list-events').then(response => {
         console.log(response)
       })
   }
   const getLocationKey = async () => {
-    await axios.get('http://dataservice.accuweather.com/locations/v1/cities/search?apikey=b9qlnPQZvXqa7GdY9JTN9aqQoISsyeHi&q=tel%20aviv')
+    await axios.get(`http://dataservice.accuweather.com/locations/v1/cities/search?apikey=b9qlnPQZvXqa7GdY9JTN9aqQoISsyeHi&q=${location}`)
     .then(response => {
       console.log(response.data)
     })
@@ -19,6 +27,7 @@ function App() {
       <p className='text-[25px] font-bold m-10'> Google Calendar API </p>
       <button className='text-white border-2 p-2 rounded-xl bg-blue-500' onClick={getEvents}> Fetch Events </button>
       <p className='text-[25px] font-bold m-10'> AccuWeather API </p>
+      <input type='text' placeholder='Enter location...'  onChange={onLocationChange} className='border-2 p-2 rounded-xl'/>
       <button disabled className='text-white border-2 p-2 rounded-xl bg-blue-500' onClick={getLocationKey}> Fetch Location Key </button>
     </div>
   );
